95c9deed937a4ecc359c6573c1751ea84a32b0f4
V1c70r <261824087+v1k70rc-io@users.noreply.github.com>
2026-04-07 08:56:10 -0600

    Creacion de la funcion playRound()
    Esta funcion contine toda la logica del juego

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let opcion = Math.random();
@@ -14,9 +15,39 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     answer = prompt("piedra, papel o tijeras?")
+    answer = answer.toLowerCase();
 
-    return answer.toLoweCase();
+    return answer;
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    console.log(`Eleccion del humano: ${humanChoice}`);
+    console.log(`Eleccion de la computadora: ${computerChoice}`);
+
+    if(
+        (humanChoice === 'piedra' && computerChoice === 'tijeras') ||
+        (humanChoice === 'papel' && computerChoice === 'piedra') ||
+        (humanChoice === 'tijeras' && computerChoice === 'papel') 
+    ) {
+        humanScore++;
+        console.log('Punto para el humano');
+    } else if (humanChoice === computerChoice) {
+        console.log('Es un empate')
+    } else {
+        computerScore++;
+        console.log('Punto para la computadora');
+    }
+
+    console.log(`Puntos del humano: ${humanScore}`);
+    console.log(`Puntos para la computadora ${computerScore}`);
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 
 
