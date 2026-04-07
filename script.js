3289582732c4f9643ff73f1e2ec0c7ebdf955070
V1c70r <261824087+v1k70rc-io@users.noreply.github.com>
2026-04-07 08:05:30 -0600

    Añadiendo función getComputerChoice
    Esta función genera un numero random entre 0 y 1 y retorna piedra, papel o tijeras dependiendo el valor generado.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 
 
 console.log(`Hello World`)
+
+function getComputerChoice() {
+    let opcion = Math.random();
+
+    if (opcion < 0.33) {
+        return 'Rock';
+    } else if (opcion < 0.66) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
